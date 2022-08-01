6ac15d9bdf36b72b2eb50b350dfa71d14250ec34
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 13:28:01 +0530

    Equality Operator

---
+++ 19-EqualityOperator.js
@@ -0,0 +1,15 @@
+let x = 1;
+
+// Striclty Equality [===]
+console.log(1 === 1); // true
+
+console.log("1" === 1); // false
+
+// Loose Equality [==]
+console.log(1 == 1); // true
+
+console.log("1" == 1); // true
+
+console.log(true == 1); // true
+
+
