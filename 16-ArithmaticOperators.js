ffb66e2d474527035692e062c819c3ff626dacca
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 13:06:43 +0530

    Arithmatic Operator

---
+++ 16-ArithmaticOperators.js
@@ -0,0 +1,16 @@
+let x = 20;
+let y = 10;
+
+// x + y is an expression
+console.log(x + y); // Addition
+console.log(x - y); // Subtraction
+console.log(x / y); // Division
+console.log(x * y); // Multiplication
+console.log(x % y); // Modules (gives remainder)
+console.log(x ** y); // Exponential x to the power of y
+
+console.log(++x); // Pre Increment
+console.log(x++); // post Increment
+
+console.log(--x); // Pre Decrement
+console.log(x--); // Post Decrement
