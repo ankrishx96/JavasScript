99832f253b2b1f2c4d171377f92a5789734e2bfa
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 13:22:14 +0530

    Comparison Operator

---
+++ 18-ComparisonOperator.js
@@ -0,0 +1,14 @@
+let x = 1;
+
+//Relational Operators
+console.log(x > 0); // Greater than
+console.log(x >= 0); // Greater than equal to
+
+console.log(x < 0); // Less than
+console.log(x <= 0); // Less than equal to
+
+//Equallity Operator
+console.log(x == 1); // Value Checking
+console.log(x != 1); // Reverse of value checking
+console.log(x === 1); // Value as well as type checking
+console.log(x !== 1); // Reverse of Strictly checking
