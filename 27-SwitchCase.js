581d8fcd7d7199e745c202b269f738acab4f6ab6
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 16:02:24 +0530

    Switch Case

---
+++ 27-SwitchCase.js
@@ -0,0 +1,18 @@
+let role = "ankrish";
+
+switch (role) {
+  case "guest":
+    console.log("Welcome Guest");
+    break;
+
+  case "moderator":
+    console.log("Welcome Moderator");
+    break;
+
+  case "admin":
+    console.log("Welcome Admin");
+    break;
+
+  default:
+    console.log("You are not authorized");
+}
