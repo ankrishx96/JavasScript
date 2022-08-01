4871e1e1a964bc68f0bcd05a7b2f245d5a914540
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 11:52:13 +0530

    Primitive / Value Types

---
+++ 09-PrimitiveDataType.js
@@ -0,0 +1,9 @@
+let name = "Ankrish"; // String Primitive DataType
+
+let age = "25"; // Number Primitive DataType
+
+let isLoggedIn = true; // Boolean Primitive DataType value can be (true | false)
+
+let lastName; //Undefined Primitive DataType
+
+let fullName = null; //Null Primitive DataType - It is used to clear the value of a variable
