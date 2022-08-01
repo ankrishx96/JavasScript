22dd93b7b5a82722b83dd2c353bcbac4cabb9e22
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 12:24:29 +0530

    Arrays

---
+++ 12-Array.js
@@ -0,0 +1,11 @@
+let selectedColors = ["red", "blue"]; // Creating Array
+
+selectedColors[2] = "yellow"; // Adding element dynamically
+
+console.log(selectedColors); // Access the whole array
+
+console.log(selectedColors[0]); // Access index 0 which is 1st item in array
+
+console.log(selectedColors[2]); // Access index 2 which is 2nd item in array
+
+console.log("Length of Array:" + selectedColors.length);
