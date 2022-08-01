7c9f130a1346a5c9104d2386844f0e564d0c92e1
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 11:36:24 +0530

    Declaration of Variables & Rules for naming it.

---
+++ 07-Variables.js
@@ -0,0 +1,20 @@
+let name = "Javascript";
+
+// Rules for declaration of variables.
+// 1. Cannot be a reserved keywords. (let | if | else)
+// 2. Should be meaningfull labels.
+// 3. Cannot Start with a number.
+// 4. Cannot contain space / hypen(-).
+// 5. Variables are case sensitive.
+
+console.log(name);
+
+
+//Avoid this
+let firstName = "Ankrish",
+  lastName = "Kandpal"; // Multiple Variables Declaration
+
+
+//Best Practice is to declare variable in different lines
+let myFirstName = "Ankrish";
+let myLastName = "Kandpal";
