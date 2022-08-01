44fc009b9f0321488dde620dfdbf2364451ffa84
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 11:45:52 +0530

    Constants

---
+++ 08-Constants.js
@@ -0,0 +1,9 @@
+let name = "Ankrisj";
+name = "Ankrish";
+
+console.log(name); // Working because name can be changed during runtime.
+
+const pi = 3.14;
+pi = 3.1415;
+
+console.log(pi); // Throw error Assignment to constant variable.
