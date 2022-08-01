58a8342aa536d74830d545235e68e01aaf5daeb7
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 12:02:29 +0530

    Dynamic Typing

---
+++ 10-DynamicTyping.js
@@ -0,0 +1,7 @@
+let value = "Ankrish"; // Can be String | Number | Boolean | undefined | null based on the data we passed.
+
+console.log(typeof value); // Initially the type is String
+
+value = 123;
+
+console.log(typeof value); // After it changed it to Number
