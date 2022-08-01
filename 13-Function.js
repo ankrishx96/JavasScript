772d92822c4217120ac8d651e3004a34ce90d7bf
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 12:48:17 +0530

    Function

---
+++ 13-Function.js
@@ -0,0 +1,8 @@
+// Function Declaration
+function greet(fname, lname) {
+  console.log("Hello " + fname + " " + lname);
+}
+
+greet("Ankrish", "Kandpal"); // Function call
+
+// fname is paramater & 'Ankrish' is an argument
