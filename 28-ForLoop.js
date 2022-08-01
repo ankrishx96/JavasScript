cb21844f5bc5e831ee64fab39102637b5eac50b6
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-01 16:10:32 +0530

    For loop

---
+++ 28-ForLoop.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 5; i++) {
+  console.log(`Welcome ${i} times`);
+}
+//Welcome 0 times Welcome 1 times Welcome 2 times Welcome 3 times Welcome 4 times
+
+for (let i = 0; i < 5; i++) {
+  if (i % 2 == 0) console.log(i);
+}
+// 0 2 4
