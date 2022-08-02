3b85936b61617d6021f213c5cbe596290ee172d8
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-02 17:23:04 +0530

    Create a price range object

---
+++ 65-Exercise.js
@@ -0,0 +1,26 @@
+const priceRange = [
+  {
+    label: "$",
+    tooltip: "Inexpensive",
+    minPrice: 0,
+    maxPrice: 10,
+  },
+  {
+    label: "$$",
+    tooltip: "Moderate",
+    minPrice: 11,
+    maxPrice: 28,
+  },
+  {
+    label: "$$",
+    tooltip: "Expensive",
+    minPrice: 21,
+    maxPrice: 50,
+  },
+];
+
+let restaurants = [
+  {
+    averagePerPerson: 5,
+  },
+];
