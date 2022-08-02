a812250d7652746a066410fc455a210eda56beee
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-02 17:35:48 +0530

    Finding primitive elements in an array

---
+++ 68-FindingElements.js
@@ -0,0 +1,12 @@
+const number = [3, 4, 5, 1, 1];
+
+// Index Of indexOf(what, position)
+console.log(number.indexOf(1, 4)); // 4
+console.log(number.indexOf(1)); // 3
+console.log(number.indexOf("a")); // -1
+
+// LastIndexOf
+console.log(number.lastIndexOf(1)); // 4
+
+// includes
+console.log(number.includes(1)); // true
