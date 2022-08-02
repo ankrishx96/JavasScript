01ee479f3967d901e0045a3ac5c677c383e2c913
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-02 18:06:05 +0530

    Arrow Function

---
+++ 70-ArrowFunction.js
@@ -0,0 +1,9 @@
+// Regular Function
+function SayMyName(name) {
+  console.log(`My name is ${name}`);
+}
+
+// Arrow Function
+let SayMyName1 = (name) => console.log(`My name is ${name}`);
+
+SayMyName1("Ankrish");
