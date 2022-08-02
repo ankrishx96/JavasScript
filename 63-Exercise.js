8463d1477b27cc10619799c939f2e894c6ebfbc5
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-02 17:11:55 +0530

    Create an object post

---
+++ 63-Exercise.js
@@ -0,0 +1,13 @@
+const post = {
+  title: "A Lone Bird",
+  body: "All about birds",
+  author: "John",
+  views: 1000,
+  comments: [
+    { author: "Spark", body: "Good" },
+    { author: "Junior", body: "Must Read" },
+  ],
+  isLive: true,
+};
+
+console.log(post);
