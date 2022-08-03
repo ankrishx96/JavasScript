6ee792dd3e16070450bac1849591b5840fe3b6b0
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-03 10:39:20 +0530

    Joining self array join method

---
+++ 76-JoiningArray.js
@@ -0,0 +1,10 @@
+const number = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const joined = number.join(",");
+console.log(joined); // 1,2,3,4,5,6,7,8 // Return a string
+
+// Spliting
+const message = "Hey my name is ankrish";
+const parts = message.split(" ");
+
+console.log(parts); // [ 'Hey', 'my', 'name', 'is', 'ankrish' ]
