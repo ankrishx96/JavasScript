f740f4675f89fb6fa9c4fda94d9943e9cffe9e3c
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-03 14:35:05 +0530

    Function Declaration & Function Expression

---
+++ 89-FunctionDeclaration&Expression.js
@@ -0,0 +1,17 @@
+// Function Declaration
+function walk() {
+  console.log("Walk");
+}
+
+// Function Expression
+let fn = function () {};
+
+// Anonymous Function Expression
+let run = function () {
+  console.log("Anonymous Function Expression");
+};
+
+// Named Function Expression
+let func = function walk() {
+  console.log("Named Walk Function Expression");
+};
