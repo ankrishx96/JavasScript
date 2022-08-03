f1b0b44f675164f6160bde37ff2c0632c8c9969b
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-03 10:11:38 +0530

    Combining array into one array

---
+++ 73-CombiningArray.js
@@ -0,0 +1,9 @@
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second);
+
+console.log(combined);
+
+// Method 2
+console.log(combined.slice(2, 4)); // [ 3 , 4 ]
