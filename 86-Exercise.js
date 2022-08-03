aca9a2dac3dee66b311994bdeaea4f04baf54785
Ankrish <ankrishkandpal1996@gmail.com>
2022-08-03 13:58:06 +0530

    WAP to find the occurance of element in an array

--- 86-Exercise.js
+++ 86-Exercise.js
@@ -10,3 +10,12 @@ let countOccurrences = (arr, searchEle) => {
 
 let result = countOccurrences([1, 2, 3, 4, 1, 2, 1], 1);
 console.log(result); //3
+
+// -------------------
+
+let countOccurrences1 = (arr, searchEle) => {
+  return arr.reduce((acc, curr) => {
+    let count = curr == searchEle ? 1 : 0;
+    return acc + curr;
+  }, 0);
+};
